29da185fa52bbbd91fb145fc335e79dd46c49938
Pavel Golenkov <pgolenkov@gmail.com>
2020-05-15 11:12:47 +0500

    Add square of triangle task

---
+++ task1.js
@@ -0,0 +1,8 @@
+side = prompt("Введите длину стороны равностороннего треугольника", "");
+side = parseFloat(side);
+
+if (side && side > 0) {
+  square = side ** 2 * Math.sqrt(3) / 4;
+  alert(`Площадь треугольника равна ${square}`);
+} else
+  alert('Длина стороны должны быть числом больше 0');
