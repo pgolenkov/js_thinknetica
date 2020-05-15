9ebe14efed5b0702b517848879a366fdda5a7cc4
Pavel Golenkov <pgolenkov@gmail.com>
2020-05-15 12:26:18 +0500

    Add invert sting task

---
+++ task6.js
@@ -0,0 +1,9 @@
+input_text = prompt("Введите слово", "");
+let output_text = "";
+
+for (let i = input_text.length - 1; i >= 0; i--) {
+  symbol = input_text[i];
+  output_text += symbol;
+}
+
+alert(`Инвертированный результат: ${output_text}`);
