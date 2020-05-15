0adca5ef053f9e6a4167144a9f510e0a3ebc3b18
Pavel Golenkov <pgolenkov@gmail.com>
2020-05-15 12:19:46 +0500

    Add consonants and vowels task

---
+++ task5.js
@@ -0,0 +1,18 @@
+const consonants = "BCDFGHJKLMNPQRSTVWXZ";
+const vowels = "AEIOUY";
+
+let consonants_count = 0;
+let vowels_count = 0;
+
+input_text = prompt("Введите английский текст", "");
+input_text = input_text.toUpperCase();
+
+for (let i = 0; i < input_text.length; i++) {
+  symbol = input_text[i];
+  if (consonants.indexOf(symbol) >= 0)
+    consonants_count++;
+  else if (vowels.indexOf(symbol) >= 0)
+    vowels_count++;
+}
+
+alert(`Количество согласных букв: ${consonants_count}; количество гласных: ${vowels_count}`);
