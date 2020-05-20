0c491b22f583c9ea9024b462d15bc407df9f1361
Pavel Golenkov <pgolenkov@gmail.com>
2020-05-20 11:36:27 +0500

    Use reduce instead forEach for task1

--- lesson02/task1.js
+++ lesson02/task1.js
@@ -1,15 +1,9 @@
 function negative_items_sum_and_count(array) {
-    let count = 0;
-    let sum = 0;
+    const result = array.reduce((acc, item) => {
+        return item < 0 ? { count: acc.count + 1, sum: acc.sum + item } : acc
+    }, { count: 0, sum: 0 });
 
-    array.forEach(item => {
-        if (item < 0) {
-            count++;
-            sum += item;
-        }
-    });
-
-    return { count, sum };
+    return result;
 }
 
 const array = [91, 93, 45, -67, -96, -40, 34, -96, 42, -58];
