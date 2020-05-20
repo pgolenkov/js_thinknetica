75ce1416ecd0ac0a09229f66767f44fb60554714
Pavel Golenkov <pgolenkov@gmail.com>
2020-05-20 13:07:34 +0500

    A bit different refactor with reduce

--- lesson02/task1.js
+++ lesson02/task1.js
@@ -1,9 +1,7 @@
 function negative_items_sum_and_count(array) {
-    const result = array.reduce((acc, item) => {
-        return item < 0 ? { count: acc.count + 1, sum: acc.sum + item } : acc
-    }, { count: 0, sum: 0 });
-
-    return result;
+    const negative_array = array.filter(item => item < 0);
+    const sum = negative_array.reduce((acc, item) => acc + item);
+    return { count: negative_array.length, sum }
 }
 
 const array = [91, 93, 45, -67, -96, -40, 34, -96, 42, -58];
