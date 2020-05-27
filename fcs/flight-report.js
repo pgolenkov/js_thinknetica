c812d7c9fca7db4b4ad09b773d6d2532869e3d5e
Pavel Golenkov <pgolenkov@gmail.com>
2020-05-27 12:18:41 +0500

    Add flightReport function with tests

--- fcs/flight-report.js
+++ fcs/flight-report.js
@@ -14,5 +14,28 @@
  * @returns {Report} отчет
  */
 function flightReport(world, flight, nowTime) {
+    if (!world.flights[flight.name])
+        throw new Error('Flight not found');
 
+    const timeToFly = (flight.registrationEnds - nowTime) / 1000 / 3600;
+    const registration = timeToFly > 1 && timeToFly < 5;
+
+    const complete = timeToFly < 1;
+
+    const countOfSeats = flight.seats;
+
+    const reservedSeats = flight.tickets.length;
+
+    const registeredSeats = flight.tickets.filter(t => t.registrationTime).length
+
+    const report = {
+        flight: flight.name,
+        registration,
+        complete,
+        countOfSeats,
+        reservedSeats,
+        registeredSeats
+    }
+
+    return report;
 }
---
+++ fcs/flight-report.test.js
@@ -0,0 +1,122 @@
+describe('flightReport', () => {
+    let bigWorld = {
+        flights: {},
+        history: [],
+    };
+
+    before(() => {
+        let result = addFlight(
+            bigWorld,
+            {
+                name: 'Airbus 747',
+                seats: 36,
+                businessSeats: 4,
+            },
+            makeTime(16, 0),
+            'BH118'
+        );
+        bigWorld = result.world;
+    });
+
+    describe('when no tickets were bought', () => {
+        it('returns Report object with no reserved seats', () => {
+            const flight = bigWorld.flights['BH118'];
+            let result = flightReport(bigWorld, flight, makeTime(12, 0));
+            const expectedResult = {
+                flight: 'BH118',
+                registration: true,
+                complete: false,
+                countOfSeats: 36,
+                reservedSeats: 0,
+                registeredSeats: 0
+            };
+            assert.deepEqual(result, expectedResult);
+        });
+    });
+
+    describe('when ticket was bought', () => {
+        before(() => {
+            let result = buyTicket(bigWorld, 'BH118', makeTime(5, 10), 'Petrov I. I.');
+            bigWorld = result.world;
+        });
+
+        it('returns Report object when registration available', () => {
+            const flight = bigWorld.flights['BH118'];
+            let result = flightReport(bigWorld, flight, makeTime(12, 0));
+            const expectedResult = {
+                flight: 'BH118',
+                registration: true,
+                complete: false,
+                countOfSeats: 36,
+                reservedSeats: 1,
+                registeredSeats: 0
+            };
+            assert.deepEqual(result, expectedResult);
+        });
+
+        it('returns Report object when registration unavailable', () => {
+            const flight = bigWorld.flights['BH118'];
+            let result = flightReport(bigWorld, flight, makeTime(10, 0));
+            const expectedResult = {
+                flight: 'BH118',
+                registration: false,
+                complete: false,
+                countOfSeats: 36,
+                reservedSeats: 1,
+                registeredSeats: 0
+            };
+            assert.deepEqual(result, expectedResult);
+        });
+
+        it('returns Report object when registration begins', () => {
+            const flight = bigWorld.flights['BH118'];
+            let result = flightReport(bigWorld, flight, makeTime(11, 0));
+            const expectedResult = {
+                flight: 'BH118',
+                registration: true,
+                complete: false,
+                countOfSeats: 36,
+                reservedSeats: 1,
+                registeredSeats: 0
+            };
+            assert.deepEqual(result, expectedResult);
+        });
+
+        it('returns Report object when registration ends', () => {
+            const flight = bigWorld.flights['BH118'];
+            let result = flightReport(bigWorld, flight, makeTime(15, 0));
+            const expectedResult = {
+                flight: 'BH118',
+                registration: false,
+                complete: true,
+                countOfSeats: 36,
+                reservedSeats: 1,
+                registeredSeats: 0
+            };
+            assert.deepEqual(result, expectedResult);
+        });
+
+        it('returns Report object when the plane flew away', () => {
+            const flight = bigWorld.flights['BH118'];
+            let result = flightReport(bigWorld, flight, makeTime(16, 0));
+            const expectedResult = {
+                flight: 'BH118',
+                registration: false,
+                complete: true,
+                countOfSeats: 36,
+                reservedSeats: 1,
+                registeredSeats: 0
+            };
+            assert.deepEqual(result, expectedResult);
+        });
+    });
+
+    it('throws error when flight is not in world flights', () => {
+        let emptyWorld = {
+            flights: {},
+            history: [],
+        };
+        const flight = bigWorld.flights['BH118'];
+        assert.throws(() => flightReport(emptyWorld, flight, makeTime(15, 0), 'Flight not found'));
+    });
+});
