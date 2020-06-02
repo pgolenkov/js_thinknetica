7e11ffe5ec7ef871487b17b61ca982570009371a
Pavel Golenkov <pgolenkov@gmail.com>
2020-06-02 09:27:50 +0500

    Fix issues

--- lesson03_advanced/encode-text.js
+++ lesson03_advanced/encode-text.js
@@ -3,17 +3,17 @@ function encodeText(text) {
 
     const unique_words = words.filter((word, index, source) => source.indexOf(word) === index);
 
-    const dictionary = unique_words.reduce((acc, unique_word) => {
-        const count = words.filter(word => word === unique_word).length;
-        let exists;
+    const use_codes = [];
+
+    const dictionary = unique_words.map(word => {
+        const count = words.filter(w => w === word).length;
         let code;
         do {
             code = Math.random().toString(36).substr(2, 2);
-            exists = acc.some(item => item.code === code);
-        } while (exists);
-        acc.push({ word: unique_word, count, code });
-        return acc;
-    }, [])
+        } while (use_codes.some(c => c === code));
+        use_codes.push(code);
+        return { word, count, code };
+    });
 
     const encodedText = words.map(word => dictionary.find(item => item.word === word).code).join(',');
     return { dictionary, encodedText };
